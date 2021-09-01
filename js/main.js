313a4c1c4061fbcdb2c7132a272333ebc0ea030b
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-01 11:16:59 +0200

    change if in compareNumber()

--- js/main.js
+++ js/main.js
@@ -29,15 +29,15 @@ pruebaBtn.addEventListener('click', handleClickBtn);
 
 //Función para comparar el número introducido por la usuaria con el random number y que aparezca la pista correspondiente.
 function compareNumber() {
-const getInput = number.value;
-    if (getInput >= randomNumber) {
+const getInput = parseInt(number.value);
+    if(getInput <0 || getInput > 100) {
+       clue.innerHTML = 'Pista: El número debe estar entre 1 y 100' 
+    } else if (getInput > randomNumber) {
         clue.innerHTML = 'Pista: Demasiado alto'
-    } else if (getInput <= randomNumber) {
+    } else if (getInput < randomNumber) {
         clue.innerHTML = 'Pista: Demasiado bajo'
-    } else if (getInput === randomNumber){
-        clue.innerHTML = 'Pista: ¡Has ganado campeona!'
     } else {
-        clue.innerHTML = 'Pista: El número debe estar entre 1 y 100'
+        clue.innerHTML = 'Pista: ¡Has ganado campeona!'
     }
 }
 
