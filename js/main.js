bdbf9bfdbd39ccd453f4dad1f3f2077c0601e2d6
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-01 10:38:53 +0200

    add const getInput inside function compareNumber; and add compareNumber inside handleclick..

--- js/main.js
+++ js/main.js
@@ -9,34 +9,36 @@ const clue = document.querySelector('.js-clue');
 
 const randomNumber = getRandomNumber();
 
-const getInput = number.value;
-
 //Función para coger número aleatorio
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * 100);
    }
 
-   console.log('Mi número aleatorio es ' + getRandomNumber())
+   console.log('Mi número aleatorio es ' + randomNumber)
 
 //Función para hacer que el número escrito en el input aparezca en la consola.
  
 function handleClickBtn() {
     const inputNumber = number.value;
     console.log(inputNumber);
+    compareNumber()
 }
 
 pruebaBtn.addEventListener('click', handleClickBtn);
 
-//
+//Función para comparar el número introducido por la usuaria con el random number y que aparezca la pista correspondiente.
 function compareNumber() {
+const getInput = number.value;
     if (getInput >= randomNumber) {
-        clue.innerHTML = 'Demasiado alto'
+        clue.innerHTML = 'Pista: Demasiado alto'
     } else if (getInput <= randomNumber) {
         clue.innerHTML = 'Pista: Demasiado bajo'
-    } else if (getInput !== randomNumber){
-        clue.innerHTML = 'Pista: El número debe estar entre 1 y 100'
-    } else {
+    } else if (getInput === randomNumber){
         clue.innerHTML = 'Pista: ¡Has ganado campeona!'
+    } else {
+        clue.innerHTML = 'Pista: El número debe estar entre 1 y 100'
     }
 }
-compareNumber();
+
+
+//Actualizar el contador de intentos cada vez que la jugadora pruebe
