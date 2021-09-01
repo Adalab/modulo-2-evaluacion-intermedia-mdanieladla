cc945f823e62fbc891a243488ffdddfc9b8f361e
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-01 09:29:00 +0200

    add event listener for button

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 //Variables
 const number = document.querySelector('.js-input');
 
-const prueba = document.querySelector('.js-btn');
+const pruebaBtn = document.querySelector('.js-btn');
 
 //Función para coger número aleatorio
 function getRandomNumber(max) {
@@ -12,4 +12,10 @@ function getRandomNumber(max) {
 
    console.log('Mi número aleatorio es ' + getRandomNumber())
 
+//Función para hacer que el número escrito en el input aparezca en la consola.
+function handleClickBtn() {
+    const inputNumber = number.value;
+    console.log(inputNumber);
+}
 
+pruebaBtn.addEventListener('click', handleClickBtn);
