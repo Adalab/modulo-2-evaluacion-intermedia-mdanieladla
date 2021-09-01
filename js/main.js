d0fa4a960aa0a69f34d2bf103a60b11afa045942
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-01 09:55:55 +0200

    add more variables and function compare number

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@ const number = document.querySelector('.js-input');
 
 const pruebaBtn = document.querySelector('.js-btn');
 
+const clue = document.querySelector('.js-clue');
+
+const randomNumber = getRandomNumber();
+
+const getInput = number.value;
+
 //Función para coger número aleatorio
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * 100);
@@ -13,9 +19,24 @@ function getRandomNumber(max) {
    console.log('Mi número aleatorio es ' + getRandomNumber())
 
 //Función para hacer que el número escrito en el input aparezca en la consola.
+ 
 function handleClickBtn() {
     const inputNumber = number.value;
     console.log(inputNumber);
 }
 
 pruebaBtn.addEventListener('click', handleClickBtn);
+
+//
+function compareNumber() {
+    if (getInput >= randomNumber) {
+        clue.innerHTML = 'Demasiado alto'
+    } else if (getInput <= randomNumber) {
+        clue.innerHTML = 'Pista: Demasiado bajo'
+    } else if (getInput !== randomNumber){
+        clue.innerHTML = 'Pista: El número debe estar entre 1 y 100'
+    } else {
+        clue.innerHTML = 'Pista: ¡Has ganado campeona!'
+    }
+}
+compareNumber();
