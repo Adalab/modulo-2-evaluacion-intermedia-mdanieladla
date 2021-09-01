ebf5ebb480a87f424f4e52379b79c4dd87faff45
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-01 09:25:24 +0200

    add variables and function random number

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 'use strict'
+
+//Variables
+const number = document.querySelector('.js-input');
+
+const prueba = document.querySelector('.js-btn');
+
+//Función para coger número aleatorio
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * 100);
+   }
+
+   console.log('Mi número aleatorio es ' + getRandomNumber())
+
+
