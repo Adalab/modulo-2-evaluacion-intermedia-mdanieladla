9eb06715f38dc9a9a3d94451bca4757a0a0c9f3f
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-05 20:00:20 +0200

    fixed issues

--- js/main.js
+++ js/main.js
@@ -1,48 +1,64 @@
 'use strict'
 
 //Variables
-const number = document.querySelector('.js-input');
+const getNumberInput = document.querySelector('.js-input');
 
-const pruebaBtn = document.querySelector('.js-btn');
+const btnTest = document.querySelector('.js-btn');
 
 const clue = document.querySelector('.js-clue');
 
-const randomNumber = getRandomNumber();
+const form = document.querySelector('.js-form');
 
-const intentos = document.querySelector('.js-intentos');
+const attempts = document.querySelector('.js-attempts');
+
+let count = 0;
+
+const randomNumber = getRandomNumber(100);
 
 //Función para coger número aleatorio
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * 100);
+    return Math.ceil(Math.random() * max);
    }
 
    console.log('Mi número aleatorio es ' + randomNumber)
 
 //Función para hacer que el número escrito en el input aparezca en la consola.
 function handleClickBtn() {
-    const inputNumber = number.value;
+    const inputNumber = getNumberInput.value;
     console.log(inputNumber);
-    compareNumber()
+    compareNumber();
+    //Actualizar el contador de intentos cada vez que la jugadora pruebe
+    attempts.innerHTML = `Número de intentos: ${count++}`;
 }
 
-pruebaBtn.addEventListener('click', handleClickBtn);
+btnTest.addEventListener('click', handleClickBtn);
+
+//Función para que no se refresque la página al pulsar el botón de prueba.
+function handleSubmit(ev){
+    ev.preventDefault();
+}
+
+form.addEventListener('submit', handleSubmit);
+
+//Función para hacer más corto el código a la hora de poner en el if las pistas.
+function writeClue(message) {
+    clue.innerHTML = message
+}
 
 //Función para comparar el número introducido por la usuaria con el random number y que aparezca la pista correspondiente.
 function compareNumber() {
-const getInput = parseInt(number.value);
+const getInput = parseInt(getNumberInput.value);
     if(getInput <0 || getInput > 100) {
-       clue.innerHTML = 'Pista: El número debe estar entre 1 y 100' 
+       writeClue('Pista: El número debe estar entre 1 y 100'); 
     } else if (getInput > randomNumber) {
-        clue.innerHTML = 'Pista: Demasiado alto'
+        writeClue('Pista: Demasiado alto');
     } else if (getInput < randomNumber) {
-        clue.innerHTML = 'Pista: Demasiado bajo'
+        writeClue('Pista: Demasiado bajo');
     } else {
-        clue.innerHTML = 'Pista: ¡Has ganado campeona!'
+        writeClue('¡Has ganado campeona!');
     }
 }
 
-
-//Actualizar el contador de intentos cada vez que la jugadora pruebe
 
 
 
